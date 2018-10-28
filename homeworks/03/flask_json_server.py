28fd3b8583a4ddcf96493e2e058fd7add2991dc2
Anastasia Shirshova <orhideya7700@mail.ru>
2018-10-28 18:36:19 +0300

    Сделана третья домашка

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -1,17 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
-    #TODO прочитать из полученного запроса json-контент
-    #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
-    #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
+    data = {'version': int(version)}
+    if request.method == 'POST':
+        arg = request.json
+        if version == '1':
+            data['predict'] = arg['predict']
+        if version == '0':
+            data['predict'] = arg['old_predict'] 
+        return jsonify(data)
 
 @app.route("/")
 def hello():
-    #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return render_template('Help.html')
+
 
 if __name__ == "__main__":
     app.run()
